275598c45a60c901f9e6d3e3bba367c0e17d50d8
Manuelly1 <manuellyvictor2000@gmail.com>
2025-11-04 09:32:15 -0300

    Primeiro exercício aplicando cte

---
+++ class_exercises/17_cte.sql
@@ -0,0 +1,46 @@
+-- Objetivo: Comparar os clientes que compraram no primeiro dia com os que compraram no último dia, identificando quais 
+-- clientes do primeiro dia também aparecem (ou não) no último dia
+
+-- CTE: Commom Table Expression
+
+-- Explicações:
+-- O comando WITH é utilizado para criar uma CTE, ou seja, uma "tabela temporária nomeada" que pode ser referenciada dentro da 
+-- mesma consulta SQL;
+-- No exemplo abaixo, foram criadas duas CTEs:
+--
+-- I. tb_cliente_primeiro_dia → armazena os IDs dos clientes que realizaram transações no dia 2025-08-25 (primeiro dia);
+-- II. tb_cliente_ultimo_dia → armazena os IDs dos clientes que realizaram transações no dia 2025-08-29 (último dia).
+--
+-- Em seguida, a consulta principal faz um LEFT JOIN entre as duas CTEs, relacionando os clientes do primeiro dia (t1) com os do 
+-- último dia (t2), com base no campo IdCliente;
+-- Dessa forma, é possível identificar quais clientes compraram no primeiro dia e verificar se também realizaram transações no 
+-- último dia.
+
+-- Por que é mais interessante usar CTEs do que subqueries?
+-- Porque as CTEs permitem dividir o problema em etapas lógicas: primeiro filtramos uma parte da base (clientes do primeiro dia),
+-- depois filtramos outra (clientes do último dia) e, por fim, cruzamos os resultados de forma organizada e legível. Além disso, 
+-- o uso de CTEs torna o código mais limpo, fácil de manter e reutilizar em consultas mais complexas, em vez de aninhar 
+-- várias subqueries difíceis de entender.
+
+WITH tb_cliente_primeiro_dia AS (
+
+    SELECT DISTINCT IdCliente
+    FROM transacoes
+    WHERE substr(DtCriacao, 1, 10) = '2025-08-25'
+
+),
+
+tb_cliente_ultimo_dia AS (
+
+    SELECT DISTINCT IdCliente
+    FROM transacoes
+    WHERE substr(DtCriacao, 1, 10) = '2025-08-29'
+
+)
+
+SELECT *
+FROM tb_cliente_primeiro_dia AS t1
+
+LEFT JOIN tb_cliente_ultimo_dia AS t2
+    ON t1.IdCliente = t2.IdCliente;
+
