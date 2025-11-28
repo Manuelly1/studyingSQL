b9c5b09a576536a509f5917ebc658a60b0bf0d0f
Manuelly1 <manuellyvictor2000@gmail.com>
2025-11-28 11:05:09 -0300

    primeira questão de subquery de nível avançado

--- chat_exercises3/subqueries/q11.sql
+++ chat_exercises3/subqueries/q11.sql
@@ -3,3 +3,33 @@
 - Objetivo: Listar os clientes cujo valor total gasto é maior que o maior gasto entre clientes que fizeram apenas 1 pedido
 
 */
+
+SELECT t1.nome,
+       sum(t2.valor_total) AS totalGasto
+
+FROM clientes1 AS t1
+
+INNER JOIN pedidos1 AS t2
+    ON t1.id_cliente = t2.id_cliente
+
+GROUP BY t1.id_cliente, t1.nome
+
+HAVING sum(t2.valor_total) > (
+
+    SELECT max(total)
+
+    FROM (
+    
+        SELECT id_cliente,
+               sum(valor_total) AS total,
+               count(id_pedido) AS qtdePedidos
+
+        FROM pedidos1
+
+        GROUP BY id_cliente 
+
+        HAVING count(id_pedido) = 1
+
+    ) sub
+
+);
