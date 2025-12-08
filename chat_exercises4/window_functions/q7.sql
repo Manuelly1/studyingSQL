0d0927a2271df327f92606cd5e962c4fa5e98113
Manuelly1 <manuellyvictor2000@gmail.com>
2025-12-08 10:48:50 -0300

    ranking dos produtos

---
+++ chat_exercises4/window_functions/q7.sql
@@ -0,0 +1,16 @@
+/*
+
+- Objetivo: Na tabela Produtos, liste: produto, vendas_totais e ranking dos produtos, do maior para o menor
+            
+*/
+
+SELECT t1.nome_produto AS produto,
+       sum(t2.quantidade * t1.preco) AS vendasTotais,
+       RANK() OVER (ORDER BY sum(t2.quantidade * t1.preco) DESC) AS ranking
+
+FROM produtosE AS t1
+
+LEFT JOIN itens_pedidoE AS t2
+    ON t1.id_produto = t2.id_produto
+
+GROUP BY t1.nome_produto;
