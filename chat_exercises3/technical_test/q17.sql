2bb34758c368d57c499aeb352933d9b62f6ca008
Manuelly1 <manuellyvictor2000@gmail.com>
2025-12-13 10:22:58 -0300

    questão com subquery

---
+++ chat_exercises3/technical_test/q17.sql
@@ -0,0 +1,35 @@
+/*
+
+- Objetivo: Listar o nome do cliente, o total gasto e a quantidade de pedidos, apenas para clientes cujo total gasto seja maior que a média de 
+gasto de todos os clientes
+    
+*/ 
+
+SELECT t1.nome,
+       sum(t2.valor_total) AS totalGasto,
+       count(t2.id_pedido) AS quantidadePedidos
+
+FROM clientes1 AS t1
+
+INNER JOIN pedidos1 AS t2
+    ON t1.id_cliente = t2.id_cliente
+
+GROUP BY t1.id_cliente, t1.nome
+
+HAVING sum(t2.valor_total) > (
+
+    SELECT avg(totalGastoCliente)
+    FROM (
+
+        SELECT t3.id_cliente,
+               sum(t4.valor_total) AS totalGastoCliente
+
+        FROM clientes1 AS t3
+
+        INNER JOIN pedidos1 AS t4
+            ON t3.id_cliente = t4.id_cliente
+
+        GROUP BY t3.id_cliente
+        
+    ) sub
+);
