e8bff45962b13c0f05344c88a906d8188e89f741
Manuelly1 <manuellyvictor2000@gmail.com>
2025-12-15 14:30:27 -0300

    mais uma com subquery

--- chat_exercises5/q14.sql
+++ chat_exercises5/q14.sql
@@ -4,4 +4,16 @@
 
 */ 
 
+SELECT c1.nome_curso,
+       c1.categoria,
+       c1.preco
+       
+FROM cursos AS c1
 
+WHERE c1.preco = (
+
+    SELECT max(c2.preco)
+    FROM cursos AS c2
+    WHERE c2.categoria = c1.categoria
+
+);
