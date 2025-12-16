c06bf691cf6935df5180d6eee332bfb5aec78af9
Manuelly1 <manuellyvictor2000@gmail.com>
2025-12-16 15:37:19 -0300

    mais uma com ranking

---
+++ chat_exercises5/q22.sql
@@ -0,0 +1,19 @@
+/*
+
+- Objetivo: Gerar: nome do aluno, total gasto e ranking (maior gasto → menor gasto)
+    
+*/ 
+
+SELECT a.nome,
+       sum(c.preco) AS totalGasto,
+       DENSE_RANK() OVER (ORDER BY sum(c.preco) DESC) AS ranking
+
+FROM alunos AS a
+
+INNER JOIN matriculas AS m
+    ON a.id_aluno = m.id_aluno
+
+INNER JOIN cursos AS c
+    ON m.id_curso = c.id_curso
+    
+GROUP BY a.id_aluno, a.nome;
